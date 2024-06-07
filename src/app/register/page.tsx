935d7a05317579ce62ef95595d754898edeb5183
jose.wexel <jose.wexel@estudiantes.utec.edu.uy>
2024-06-07 19:03:33 -0300

    chore: update language to Spanish in layout component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1>No hay registro</h1>
+    </section>
+  );
+}
